a754d4de304a9540155b7aa4319ec0633912fe04
mumo257 <festusmumo06@gmail.com>
2023-11-07 22:46:15 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCkAY7Cw2vX8r8prZzIOU-MIrZyVfE5fkE",
+    authDomain: "todo-website-7c70e.firebaseapp.com",
+    projectId: "todo-website-7c70e",
+    storageBucket: "todo-website-7c70e.appspot.com",
+    messagingSenderId: "1072411639484",
+    appId: "1:1072411639484:web:0b1124be2faaaf0a2cdab9",
+    measurementId: "G-V04XHD1CVG"
+}
