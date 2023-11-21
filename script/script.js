426029c2a6e411bff41e1710836dcc8a6d72b3a9
mumo257 <festusmumo06@gmail.com>
2023-11-21 20:59:47 +0300

    first commit all done

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCkAY7Cw2vX8r8prZzIOU-MIrZyVfE5fkE",
@@ -9,7 +13,7 @@ const firebaseConfig = {
     messagingSenderId: "1072411639484",
     appId: "1:1072411639484:web:0b1124be2faaaf0a2cdab9",
     measurementId: "G-V04XHD1CVG"
-}
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
@@ -20,9 +24,9 @@ const passwordInput = document.getElementById("password")
 const submitButton = document.getElementById("submit")
 
 // Register Input
-const SignupEmail = document.getElementById("email-signup")
+const SignupEmailIn = document.getElementById("email-signup")
 const confirmsignupEmailIn = document.getElementById("confirm-email-signup")
-const confirmsignupPassword = document.getElementById("password-signup")
+const SignupPasswordIn = document.getElementById("password-signup")
 const confirmSignupPasswordIn = document.getElementById("confirm-password-signup")
 const createAccBtn = document.getElementById("create-Acc-signup")
 
@@ -33,7 +37,52 @@ const returnBtn = document.getElementById("return-btn");
 const createacc = document.getElementById("create-acct");
 const main = document.getElementById("main");
 
-let email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignupPassword;
+let email, 
+password, 
+signupEmail, 
+signupPassword,
+confirmSignupEmail, 
+confirmSignupPassword;
+
+createAccBtn.addEventListener("click", () => {
+   var isVerified = true;
+
+   signupEmail = SignupEmailIn.value
+   confirmSignupEmail = confirmsignupEmailIn.value
+   if(signupEmail != confirmSignupEmail){
+      window.alert ("Emails do not match");
+      isVerified = false
+   }
+
+   signupPassword = SignupPasswordIn.value
+   confirmSignupPassword = confirmSignupPasswordIn.value
+   if(signupPassword != confirmSignupPassword) {
+      window.alert("Passwords do not match");
+      isVerified = false
+    } 
+
+    if (
+        signupEmail == null ||
+        confirmSignupEmail == null ||
+        signupPassword == null ||
+        confirmSignupPassword == null 
+      ) {
+        window.alert("Please fill out all the inputs");
+        isVerified = false;
+      }
+    
+    if(isVerified){
+        createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+        .then((userCredentials) => {
+            window.alert("Success! Account Created");
+            window.location = "../pages/Apply.html";
+        }).catch((error) => {
+            windows.alert("error.message");
+        });
+    }
+});
+
+submitButton.addEventListener("click", () =>{});
 
 
 // login / Register navigation functionality
